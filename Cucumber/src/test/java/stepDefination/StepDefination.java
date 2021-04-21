958d0c06a8d92e57f3ba8baecfe723ed7c7454de
SuparnaShil <suparnashil71.ssc@gmail.com>
2021-04-21 17:10:44 +0600

    feature file add

---
+++ Cucumber/src/test/java/stepDefination/StepDefination.java
@@ -0,0 +1,37 @@
+package stepDefination;
+
+
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.PendingException;
+import cucumber.api.junit.Cucumber;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+@RunWith(Cucumber.class)
+public class StepDefination {
+
+    @Given("^User is on Netbanking landing page$")
+    public void user_is_on_netbanking_landing_page() throws Throwable {
+        //code
+    }
+
+    @When("^user login into application with username and password$")
+    public void user_login_into_application_with_username_and_password() throws Throwable {
+        //code
+    }
+
+    @Then("^homepage is populated$")
+    public void homepage_is_populated() throws Throwable {
+        //code
+    }
+
+    @And("^cards are displayed  $")
+    public void cards_are_displayed() throws Throwable {
+        //code
+    }
+
+}
