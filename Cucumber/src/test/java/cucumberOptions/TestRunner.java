a388743bbb063e17b2b23906871b6ad6af3f632f
SuparnaShil <suparnashil71.ssc@gmail.com>
2021-04-23 11:49:10 +0600

    with new file edited pom.xml, select junit-4 as test runner

--- Cucumber/src/test/java/cucumberOptions/TestRunner.java
+++ Cucumber/src/test/java/cucumberOptions/TestRunner.java
@@ -1,5 +1,16 @@
 package cucumberOptions;
 
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features ="src/test/java/features/Login.feature",
+		glue= "stepDefination"
+		)
+
 public class TestRunner {
 
 }
--- Cucumber/src/test/java/stepDefination/StepDefination.java
+++ Cucumber/src/test/java/stepDefination/StepDefination.java
@@ -17,21 +17,25 @@ public class StepDefination {
     @Given("^User is on Netbanking landing page$")
     public void user_is_on_netbanking_landing_page() throws Throwable {
         //code
+    	System.out.println("given");
     }
 
     @When("^user login into application with username and password$")
     public void user_login_into_application_with_username_and_password() throws Throwable {
         //code
+    	System.out.println("when");
     }
 
     @Then("^homepage is populated$")
     public void homepage_is_populated() throws Throwable {
         //code
+    	System.out.println("then");
     }
 
     @And("^cards are displayed  $")
     public void cards_are_displayed() throws Throwable {
         //code
+    	System.out.println("and");
     }
 
 }
